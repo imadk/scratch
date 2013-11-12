533fabf2d5f5ea8d36c9b4f8090b33e21787adae
ImadK <ielkhoury@gmail.com>
2013-11-12 13:52:41 -0500

    add - stop_strip function

--- text_process.py
+++ text_process.py
@@ -11,25 +11,26 @@ from collections import OrderedDict
 #For dataframes - unused yet 
 #import pandas as pd 
 #from sklearn.feature_extraction.text import CountVectorizer         
-    
-def freq_table(ss, show_table=False):
-    """computes a frequency table for word-tokenized 
-    stemmed and stopped strings - (ss is a string)"""
 
-    nltk.data.path.append("./nltk_data/")
+
+def stop_strip(ss):
+    """Strips string ss from stopwords"""
+    nltk.data.path.append("/nltk_data/")
     sp = ss.split()
     swords = set(nltk.corpus.stopwords.words('english'))
-    #Remove stop words from ss
     for s in sp:
         if (s in swords):
             sp.remove(s)
     sp = " ".join(sp)
     return ss, sp        
     
+#def freq_table(ss, show_table=False):
+    """computes a frequency table for word-tokenized 
+    stemmed and stopped strings - (ss is a string)"""
+
     
-ss, sp = freq_table("This sentence  has  a bunch of stop words that need to be removed!")
+    
+ss, sp = stop_strip("This sentence  has  a bunch of stop words that need to be removed!")
 print ss
 print sp
-
-       
 
